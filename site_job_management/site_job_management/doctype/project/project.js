4cbbfb0f7c9d151ab5be3861d201626d8a0df9c4
indsys-dev <vijay@indsystech.com>
2026-02-13 19:48:23 +0000

    Pjoect Assign, Building and Floor Assign

--- site_job_management/site_job_management/doctype/project/project.js
+++ site_job_management/site_job_management/doctype/project/project.js
@@ -1,8 +1,33 @@
 // Copyright (c) 2026, Indsys and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Project", {
-// 	refresh(frm) {
+frappe.ui.form.on("Project", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+    onload: function(frm) {
+        frm.set_query('requester_engineer', function() {
+            return {
+                filters: {
+                    category: 'Requester Engineer'
+                }
+            };
+        });
+
+        frm.set_query('client__consultant_engineer', function() {
+            return {
+                filters: {
+                    category: 'Client / Consultant Engineer'
+                }
+            };
+        });
+
+        frm.set_query('qc_engineer', function() {
+            return {
+                filters: {
+                    category: 'QC Engineer'
+                }
+            };
+        });
+    }
+});
